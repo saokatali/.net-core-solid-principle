f7a7d28a0d36a4e5be553805e83eacbd51b8c668
Saokat  Ali <saokatali@outlook.com>
2018-12-14 11:28:12 +0530

    Added Dependency Inversion Principle

---
+++ DIP/ServiceLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidPrinciple.DIP
+{
+    public class ServiceLocator
+    {
+        Dictionary<object, object> services = new Dictionary<object, object>();
+
+        public void Register<T>(Object service)
+        {
+            this.services[typeof(T)] = service;
+        }
+
+        public T Get<T>()
+        {
+            return (T)this.services[typeof(T)];
+        }
+    }
+}
